f79d000eae88665f5c2bf2ab8377331c3350c033
Evans-N <51976431+Evans-N@users.noreply.github.com>
2020-03-25 18:46:16 -0400

    Update Modal.js
    Left Modal blank as to not break master branch.

--- front-end /src/Components/Modal.js
+++ front-end /src/Components/Modal.js
@@ -1,18 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-
-function Modal(props) {
-
-    return (
-        <div className='body-modal-show'>
-            <button className="facebook-login">Connect With Facebook</button>
-            <button className="google-login">Connect with Google</button>
-            <span>or</span>
-            <button >Sign up with email</button>
-            <div className="border-rule"></div>
-            <div>Already have an account? <button>Log In</button></div>
-        </div>
-    )
+export class Modal extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
 }
 
 export default Modal;
